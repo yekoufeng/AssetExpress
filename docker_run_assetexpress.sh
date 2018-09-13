d40ae2ce5b3cf13698aa718dabe89087b3b830bd
yekoufeng <yekoufeng@ztgame.com>
2018-09-13 17:13:15 +0800

    sh

--- docker_run_assetexpress.sh
+++ docker_run_assetexpress.sh
@@ -1,3 +1,3 @@
 docker stop AssetExpress
 docker rm AssetExpress
-docker run -d -p 8089:8089 -p 9092:9092 --name AssetExpress -v $PWD/src/AssetExpress/logs:/home/AssetExpress/src/AssetExpress/logs   122.11.58.163:5000/assetexpress:v1
+docker run -d -p 8089:8089 -p 9092:9092 --name AssetExpress -v $PWD/../logs:/home/AssetExpress/src/AssetExpress/logs -v $PWD/../cdn:/home/AssetExpress/src/AssetExpress/cdn   122.11.58.163:5000/assetexpress:v1
